79ccb0d23cce3cfe1b775e4e8d1ce0a196ea147d
jk-jumur <developer.jumur@gmail.com>
2026-05-10 22:16:44 +0600

    innerHTML added

--- script/home.js
+++ script/home.js
@@ -1,11 +1,19 @@
 const productContainer = document.getElementById("productContainer");
+
+const loading = document.getElementById("loading");
 // console.log(productContainer);
 
+let isLoading = true;
+
 const fetchProducts = ()=>{
+       isLoading = true;
+       loading.style.display = "flex";
       fetch("https://in-a5-server.vercel.app/products")
       .then((res) => res.json())
       .then((data) => {
         displayProducts(data);
+         isLoading = false;
+         loading.style.display = "none";
       });
     } 
     
@@ -16,11 +24,16 @@ const fetchProducts = ()=>{
                const card = document.createElement("div");
                card.innerHTML = `
                    <div class="bg-white  rounded-xl   drowp-shadow-lg  p-4 border-t-2 border-[#4F46E5]">
-                          <img src="https://images.pexels.com/photos/994523/pexels-photo-994523.jpeg", alt="" class="rounded-xl">
+
+                          <img src= "${product.img}"
+
+                           alt="${product.title}" 
+                           class="rounded-xl h-46 w-full object-cover" />
+
                            <div class="space-y-2 my-3">
-                               <p class="font-medium">Premium Wireless Controller</p>
-                               <p class="bg-[#4F46E5]/20  py-2 px-6 rounded-xl text-[#4F46E5]  w-fit">Men</p>
-                               <p>$79.99</p>
+                               <p class="font-medium">${product.title}</p>
+                               <p class="bg-[#4F46E5]/20  py-2 px-6 rounded-xl text-[#4F46E5]  w-fit">${product.category}</p>
+                               <p>$${product.price}</p>
                            </div>
                            <button class="bg-[#4F46E5]  text-white w-full rounded-xl py-2">View Details</button>
                            </div> `;
